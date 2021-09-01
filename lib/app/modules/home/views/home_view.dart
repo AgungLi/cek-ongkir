b10c72afef7529408fa5c7355ef02e952dae6978
Agung Laksmana Ismail <agungismail414@gmail.com>
2021-09-02 06:32:31 +0700

    Extract Province & City

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,14 +1,8 @@
-import 'dart:convert';
-
-import 'package:cekongkir/app/modules/home/province_model.dart';
-import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-
-import '../city_model.dart';
 import '../controllers/home_controller.dart';
+import 'widgets/city.dart';
+import 'widgets/province.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
@@ -21,164 +15,29 @@ class HomeView extends GetView<HomeController> {
         body: ListView(
           padding: EdgeInsets.all(20),
           children: [
-            Provinsi(),
+            Provinsi(
+              tipe: "asal",
+            ),
             Obx(
-              () => controller.hiddenKota.isTrue
+              () => controller.hiddenKotaAsal.isTrue
                   ? SizedBox()
                   : Kota(
-                      provId: controller.provId.value,
+                      provId: controller.provAsalId.value,
+                      tipe: "asal",
+                    ),
+            ),
+            Provinsi(
+              tipe: "tujuan",
+            ),
+            Obx(
+              () => controller.hiddenKotaTujuan.isTrue
+                  ? SizedBox()
+                  : Kota(
+                      provId: controller.provTujuanId.value,
+                      tipe: "tujuan",
                     ),
             ),
           ],
         ));
-  }
-}
-
-class Provinsi extends GetView<HomeController> {
-  const Provinsi({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: DropdownSearch<Province>(
-        label: "Provinsi",
-        showSearchBox: true,
-        showClearButton: true,
-        searchBoxDecoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 25,
-          ),
-          hintText: "Cari provinsi..",
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(50),
-          ),
-        ),
-        onFind: (String filter) async {
-          Uri url = Uri.parse("https://api.rajaongkir.com/starter/province");
-
-          try {
-            final response = await http.get(
-              url,
-              headers: {
-                "key": "73a68e74071d2ce17aa8d91828354447",
-              },
-            );
-
-            var data = json.decode(response.body) as Map<String, dynamic>;
-            var statusCode = data["rajaongkir"]["status"]["code"];
-
-            if (statusCode != 200) {
-              throw data["rajaongkir"]["status"]["description"];
-            }
-
-            var listAllProvince =
-                data["rajaongkir"]["results"] as List<dynamic>;
-            var models = Province.fromJsonList(listAllProvince);
-            return models;
-          } catch (e) {
-            print(e);
-            return List<Province>.empty();
-          }
-        },
-        onChanged: (prov) {
-          if (prov != null) {
-            controller.hiddenKota.value = false;
-            controller.provId.value = int.parse(prov.provinceId!);
-          } else {
-            controller.hiddenKota.value = true;
-            controller.provId.value = 0;
-          }
-        },
-        popupItemBuilder: (context, item, isSelected) {
-          return Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text(
-              "${item.province}",
-              style: TextStyle(fontSize: 18),
-            ),
-          );
-        },
-        itemAsString: (item) => item.province!,
-      ),
-    );
-  }
-}
-
-class Kota extends StatelessWidget {
-  const Kota({
-    Key? key,
-    required this.provId,
-  }) : super(key: key);
-
-  final int provId;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: DropdownSearch<City>(
-        label: "Kabupaten / kota",
-        showSearchBox: true,
-        showClearButton: true,
-        searchBoxDecoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 25,
-          ),
-          hintText: "Cari kabupaten / kota..",
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(50),
-          ),
-        ),
-        onFind: (String filter) async {
-          Uri url = Uri.parse(
-              "https://api.rajaongkir.com/starter/city?province=$provId");
-
-          try {
-            final response = await http.get(
-              url,
-              headers: {
-                "key": "73a68e74071d2ce17aa8d91828354447",
-              },
-            );
-
-            var data = json.decode(response.body) as Map<String, dynamic>;
-            var statusCode = data["rajaongkir"]["status"]["code"];
-
-            if (statusCode != 200) {
-              throw data["rajaongkir"]["status"]["description"];
-            }
-
-            var listAllCity = data["rajaongkir"]["results"] as List<dynamic>;
-            var models = City.fromJsonList(listAllCity);
-            return models;
-          } catch (e) {
-            print(e);
-            return List<City>.empty();
-          }
-        },
-        onChanged: (cityValue) {
-          if (cityValue != null) {
-            print(cityValue.cityName);
-          } else {
-            print("Tidak memilih kota / kabupaten apapun");
-          }
-        },
-        popupItemBuilder: (context, item, isSelected) {
-          return Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text(
-              "${item.type} ${item.cityName}",
-              style: TextStyle(fontSize: 18),
-            ),
-          );
-        },
-        itemAsString: (item) => "${item.type} ${item.cityName}",
-      ),
-    );
   }
 }
---
+++ lib/app/modules/home/views/widgets/city.dart
@@ -0,0 +1,100 @@
+import 'dart:convert';
+
+import 'package:cekongkir/app/modules/home/controllers/home_controller.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+import '../../city_model.dart';
+
+class Kota extends GetView<HomeController> {
+  const Kota({
+    Key? key,
+    required this.provId,
+    required this.tipe,
+  }) : super(key: key);
+
+  final int provId;
+  final String tipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: DropdownSearch<City>(
+        label: tipe == "asal"
+            ? "Kabupaten / kota asal"
+            : "Kabutapen / kota tujuan",
+        showSearchBox: true,
+        showClearButton: true,
+        searchBoxDecoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 25,
+          ),
+          hintText: "Cari kabupaten / kota..",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+        ),
+        onFind: (String filter) async {
+          Uri url = Uri.parse(
+              "https://api.rajaongkir.com/starter/city?province=$provId");
+
+          try {
+            final response = await http.get(
+              url,
+              headers: {
+                "key": "73a68e74071d2ce17aa8d91828354447",
+              },
+            );
+
+            var data = json.decode(response.body) as Map<String, dynamic>;
+            var statusCode = data["rajaongkir"]["status"]["code"];
+
+            if (statusCode != 200) {
+              throw data["rajaongkir"]["status"]["description"];
+            }
+
+            var listAllCity = data["rajaongkir"]["results"] as List<dynamic>;
+            var models = City.fromJsonList(listAllCity);
+            return models;
+          } catch (e) {
+            print(e);
+            return List<City>.empty();
+          }
+        },
+        onChanged: (cityValue) {
+          if (cityValue != null) {
+            if (tipe == "asal") {
+              controller.hiddenKotaAsal.value = false;
+              controller.provAsalId.value = int.parse(cityValue.provinceId!);
+            } else {
+              controller.hiddenKotaTujuan.value = false;
+              controller.provTujuanId.value = int.parse(cityValue.provinceId!);
+            }
+          } else {
+            if (tipe == "asal") {
+              controller.hiddenKotaAsal.value = true;
+              controller.provAsalId.value = 0;
+            } else {
+              controller.hiddenKotaTujuan.value = true;
+              controller.provTujuanId.value = 0;
+            }
+          }
+        },
+        popupItemBuilder: (context, item, isSelected) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              "${item.type} ${item.cityName}",
+              style: TextStyle(fontSize: 18),
+            ),
+          );
+        },
+        itemAsString: (item) => "${item.type} ${item.cityName}",
+      ),
+    );
+  }
+}
