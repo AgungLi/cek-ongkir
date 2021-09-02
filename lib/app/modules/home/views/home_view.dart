0a1c86e9a4844cf3bda65b30ccba5f386a9566f5
Agung Laksmana Ismail <agungismail414@gmail.com>
2021-09-02 13:24:39 +0700

    Create Logo

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -11,88 +11,90 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('HomeView'),
+          title: Text('CekOngkir'),
           centerTitle: true,
           backgroundColor: Colors.red[900],
         ),
-        body: ListView(
-          padding: EdgeInsets.all(20),
-          children: [
-            Provinsi(
-              tipe: "asal",
-            ),
-            Obx(
-              () => controller.hiddenKotaAsal.isTrue
-                  ? SizedBox()
-                  : Kota(
-                      provId: controller.provAsalId.value,
-                      tipe: "asal",
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.all(20),
+            children: [
+              Provinsi(
+                tipe: "asal",
+              ),
+              Obx(
+                () => controller.hiddenKotaAsal.isTrue
+                    ? SizedBox()
+                    : Kota(
+                        provId: controller.provAsalId.value,
+                        tipe: "asal",
+                      ),
+              ),
+              Provinsi(
+                tipe: "tujuan",
+              ),
+              Obx(
+                () => controller.hiddenKotaTujuan.isTrue
+                    ? SizedBox()
+                    : Kota(
+                        provId: controller.provTujuanId.value,
+                        tipe: "tujuan",
+                      ),
+              ),
+              BeratBarang(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: DropdownSearch<Map<String, dynamic>>(
+                  mode: Mode.MENU,
+                  showClearButton: true,
+                  showSelectedItem: false,
+                  items: [
+                    {
+                      "code": "jne",
+                      "name": "Jalur Nugraha Ekakurir (JNE)",
+                    },
+                    {
+                      "code": "tiki",
+                      "name": "Titipan Kilat (TIKI)",
+                    },
+                    {
+                      "code": "pos",
+                      "name": "Perusahan Opsional Surat (POS Indonesia)",
+                    },
+                  ],
+                  label: "Tipe Kurir",
+                  hint: "Pilih tipe kurir...",
+                  onChanged: (value) {
+                    if (value != null) {
+                      controller.kurir.value = value["code"];
+                      controller.showButton();
+                    } else {
+                      controller.hiddenButton.value = true;
+                      controller.kurir.value;
+                    }
+                  },
+                  itemAsString: (item) => "${item['name']}",
+                  popupItemBuilder: (context, item, isSelected) => Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "${item['name']}",
+                      style: TextStyle(fontSize: 18, color: Colors.black),
                     ),
-            ),
-            Provinsi(
-              tipe: "tujuan",
-            ),
-            Obx(
-              () => controller.hiddenKotaTujuan.isTrue
-                  ? SizedBox()
-                  : Kota(
-                      provId: controller.provTujuanId.value,
-                      tipe: "tujuan",
-                    ),
-            ),
-            BeratBarang(),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 50),
-              child: DropdownSearch<Map<String, dynamic>>(
-                mode: Mode.MENU,
-                showClearButton: true,
-                showSelectedItem: false,
-                items: [
-                  {
-                    "code": "jne",
-                    "name": "Jalur Nugraha Ekakurir (JNE)",
-                  },
-                  {
-                    "code": "tiki",
-                    "name": "Titipan Kilat (TIKI)",
-                  },
-                  {
-                    "code": "pos",
-                    "name": "Perusahan Opsional Surat (POS Indonesia)",
-                  },
-                ],
-                label: "Tipe Kurir",
-                hint: "Pilih tipe kurir...",
-                onChanged: (value) {
-                  if (value != null) {
-                    controller.kurir.value = value["code"];
-                    controller.showButton();
-                  } else {
-                    controller.hiddenButton.value = true;
-                    controller.kurir.value;
-                  }
-                },
-                itemAsString: (item) => "${item['name']}",
-                popupItemBuilder: (context, item, isSelected) => Container(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    "${item['name']}",
-                    style: TextStyle(fontSize: 18, color: Colors.black),
                   ),
                 ),
               ),
-            ),
-            Obx(() => controller.hiddenButton.isTrue
-                ? SizedBox()
-                : ElevatedButton(
-                    onPressed: () => controller.ongkosKirim(),
-                    child: Text("CEK ONGKOS KIRIM"),
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 20),
-                      primary: Colors.red[900],
-                    ),
-                  ))
-          ],
+              Obx(() => controller.hiddenButton.isTrue
+                  ? SizedBox()
+                  : ElevatedButton(
+                      onPressed: () => controller.ongkosKirim(),
+                      child: Text("CEK ONGKOS KIRIM"),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        primary: Colors.red[900],
+                      ),
+                    ))
+            ],
+          ),
         ));
   }
 }
